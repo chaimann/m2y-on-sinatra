bf8868faa8fc5316c329ed2fe68247d8319540a8
Eugene Chaikin <nutaksho@gmail.com>
2022-07-17 00:39:37 +0300

    prepare for heroku

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
-require "sinatra/reloader" if development?
-require "sinatra/json"
+require 'sinatra/reloader' if development?
+require 'sinatra/json'
+require_relative './lib/import_transaction'
 
 get '/callback' do
   200
