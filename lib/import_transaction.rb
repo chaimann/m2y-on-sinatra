243476b23903283a9268c2fb0866dbda03e01dc2
Eugene Chaikin <nutaksho@gmail.com>
2022-07-19 00:31:48 +0300

    update account and budget maps

--- lib/import_transaction.rb
+++ lib/import_transaction.rb
@@ -1,13 +1,22 @@
 require 'ynab'
 
 class ImportTransaction
+  BUDGETS_MAP = {
+    ENV['MONO_WHITE_ACC_ID']  => BUDGET_ID,
+    ENV['MONO_BLACK_ACC_ID']  => BUDGET_ID,
+    ENV['MONO_UAH_PE_ACC_ID'] => BUDGET_ID,
+    ENV['MONO_USD_PE_ACC_ID'] => USD_BUDGET_ID
+  }
+
   ACCOUNTS_MAP = {
-    ENV['MONO_WHITE_ACC_ID'] => ENV['YNAB_WHITE_ACC_ID'],
-    ENV['MONO_BLACK_ACC_ID'] => ENV['YNAB_BLACK_ACC_ID'],
-    ENV['MONO_USD_ACC_ID']   => ENV['YNAB_USD_ACC_ID'],
+    ENV['MONO_WHITE_ACC_ID']    => ENV['YNAB_WHITE_ACC_ID'],
+    ENV['MONO_BLACK_ACC_ID']    => ENV['YNAB_BLACK_ACC_ID'],
+    ENV['MONO_UAH_PE_ACC_ID']   => ENV['YNAB_TRANSIT_ACC_ID'],
+    ENV['MONO_USD_PE_ACC_ID']   => ENV['YNAB_USD_ACC_ID'],
   }
 
   BUDGET_ID = ENV['BUDGET_ID']
+  USD_BUDGET_ID = ENV['USD_BUDGET_ID']
 
   def initialize(account_id, item)
     @account_id = account_id
@@ -15,7 +24,7 @@ class ImportTransaction
   end
 
   def perform
-    ynab_client.transactions.create_transaction(BUDGET_ID, transaction_data)
+    ynab_client.transactions.create_transaction(BUDGETS_MAP.fetch[@account_id], transaction_data)
     true
   rescue YNAB::ApiError => e
     puts "==== ERROR: id=#{e.id}; name=#{e.name}; detail: #{e.detail}"
